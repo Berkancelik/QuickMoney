b3c3444f55d3fed4d8d76bb255792deb3b0132dc
Berkan Çelik <celikberkan1@outlook.com>
2023-05-12 22:13:47 +0300

    Add project files.

---
+++ QuickMoney/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuickMoney.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
