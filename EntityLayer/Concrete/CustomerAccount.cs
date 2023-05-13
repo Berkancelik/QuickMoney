ebf2a818c16457284bfc3f184874c28c077cf5dd
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-05-13 14:22:39 +0300

    Some Dtos created

--- EntityLayer/Concrete/CustomerAccount.cs
+++ EntityLayer/Concrete/CustomerAccount.cs
@@ -13,6 +13,8 @@ namespace EntityLayer.Concrete
         public string Currency { get; set; }
         public decimal Balance { get; set; }
         public string BankBranch { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
 
     }
 }
