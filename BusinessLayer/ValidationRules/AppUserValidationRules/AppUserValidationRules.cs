5a945f1eb5470d61d6b57423df2d77a7911d4825
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-05-15 23:03:55 +0300

    FluentValidation added

---
+++ BusinessLayer/ValidationRules/AppUserValidationRules/AppUserValidationRules.cs
@@ -0,0 +1,15 @@
+﻿using DtoLayer.DTOs.AppUserDtos;
+using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserValidationRules:AbstractValidator<AppUserRegisterDto>
+    {
+    }
+}
--- DtoLayer/DTOs/AppUserDtos/AppUserRegisterDto.cs
+++ DtoLayer/DTOs/AppUserDtos/AppUserRegisterDto.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace DtoLayer.DTOs.AppUserDtos
 {
-    internal class AppUserRegisterDto
+    public class AppUserRegisterDto
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
