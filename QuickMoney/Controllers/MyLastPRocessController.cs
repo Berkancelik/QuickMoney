ab826633d3fc4248f39f51d035b91c78abe13741
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-07-05 23:23:21 +0300

    Exchange and MyLasController fixed

---
+++ QuickMoney/Controllers/MyLastPRocessController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickMoney.Controllers
+{
+    public class MyLastPRocessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
